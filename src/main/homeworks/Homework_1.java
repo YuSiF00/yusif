6a6defc7ec7f12e9be1f2924ca3770cd86fe937e
yusif00 <yusifmemmedov553@gmail.com>
2019-10-18 23:11:58 +0400

    They are my homeworks.. Homework 1 and Homework 2

---
+++ src/main/homeworks/Homework_1.java
@@ -0,0 +1,34 @@
+package main.homeworks;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Homework_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        System.out.print("Please enter your name : ");
+        String name = sc.nextLine();
+        System.out.println("Let the game begin!");
+        int number = random.nextInt(100);
+
+       // System.out.println(number);
+        while (true) {
+            System.out.print("Please enter your number : ");
+            int your_number = sc.nextInt();
+            if(your_number < number){
+                System.out.println("Your number is too small. Please, try again.");
+            }else if(your_number>number){
+                System.out.println("Your number is too big. Please, try again.");
+            }else{
+                System.out.println("Congratulations, "+name);
+                break;
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ src/main/homeworks/Homework_2.java
@@ -0,0 +1,88 @@
+package main.homeworks;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Homework_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+
+
+        int a;
+        int b;
+        while(true){
+            a = random.nextInt(6);
+            b = random.nextInt(6);
+            if(a!=0&&b!=0){
+                break;
+            }
+        }
+
+        String[][] array = new String[6][6];
+        array[0][0] = "0";
+        array[0][1] = "1";
+        array[0][2] = "2";
+        array[0][3] = "3";
+        array[0][4] = "4";
+        array[0][5] = "5";
+        array[1][0] = "1";
+        array[2][0] = "2";
+        array[3][0] = "3";
+        array[4][0] = "4";
+        array[5][0] = "5";
+
+
+
+        for(int i = 1;i<6;i++){
+            for(int j = 1 ;j<6;j++){
+                array[i][j] = "-";
+            }
+            //System.out.println("");
+        }
+
+        for(int i =0;i<6;i++){
+            for(int j = 0;j<6;j++){
+                System.out.print(array[i][j]);
+            }
+        }
+        System.out.println("");
+
+        System.out.println("All set. Get ready to rumble!");
+        while(true){
+            int c;
+            int d;
+            while(true){
+                c = sc.nextInt();
+                d = sc.nextInt();
+                if((c>=1&&c<=5)&&(d>=1&&d<=5)){
+                    break;
+                }else{
+                    System.err.println("Your numbers must be between 1 and 5!!! ");
+                    System.out.println("Please enter your numbers again");
+                }
+            }
+
+            if(c==a&&d==b){
+                array[c][d] = "X";
+                System.out.println("You are won");
+                break;
+            }else{
+                array[c][d] = "*";
+            }
+            for(int i =0;i<6;i++){
+                for(int j = 0;j<6;j++){
+                    System.out.print(array[i][j]);
+                }
+            }
+        }
+
+        for(int i =0;i<6;i++){
+            for(int j = 0;j<6;j++){
+                System.out.print(array[i][j]);
+            }
+        }
+
+    }
+}
